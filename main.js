a1b039aaa42236b6f324fd145739a2a16cfa2e5f
FahmyDev <atepfahmyy02333@gmail.com>
2023-03-26 16:19:56 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,22 @@
+//PopUp Awal
+alert("BELAJAR JAVASCRIPT DASAR")
+
+//Nama var
+var namavar 
+function namavar(){
+    var nama = "Atep Fahmi ALbanani"
+    console.log(nama)
+}
+namavar()
+
+//Nama let
+let namalet = "Fahmi"
+
+//Nama const
+const namaconst ="Atep"
+
+//Menampilkam/Input
+
+console.log(namalet)
+console.log(namaconst)
+
