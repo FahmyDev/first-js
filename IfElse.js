9c635f4607c44039eb129f2636a95da70433c2ba
FahmyDev <atepfahmyy02333@gmail.com>
2023-04-05 07:11:18 +0700

    update tugas

---
+++ IfElse.js
@@ -0,0 +1,18 @@
+var Schools = true //JIka tipe data bernilai false, langkah ke 2 akan di lewati
+console.log("Flowchart Berangkat SEKOLAH")
+console.log("=====================================")
+console.log("1. Persiapan Sebelum Berangkat Sekolah")
+if (Schools) {
+    console.log("2. Cuci pakaian dan sepatu biar bisa sekolah") //Jika Tipe data bernilai false, maka langkah ini akan di lewati
+}
+console.log("3. Berangkat Sekolah")
+console.log("=====================================")
+
+//Jika kondisi bernilai false
+let number = 10
+if (number > 15) {
+    console.log(number)
+} else {
+    console.log("Nilai kurang dari 15")
+}
+
---
+++ LogicalOperator.js
@@ -0,0 +1,15 @@
+let x = 5
+let y = 8
+let z = 10
+
+//AND Operator
+console.log(x < 6 && y > 6) //true
+console.log(y > 7 && z > 12) //false
+
+//OR Operator
+console.log(x > 8 || y < 10) //true
+console.log(y < 10 || z > 12) //true
+
+//NOT Operator
+console.log(!(x < 8)) // false
+console.log(!(y > 10 && z < 9)) //true
