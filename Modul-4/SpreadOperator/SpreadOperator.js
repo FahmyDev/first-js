2e5961bf469c13c604ffb00eb633e810f1e1fc80
FahmyDev <atepfahmyy02333@gmail.com>
2023-04-12 14:17:44 +0700

    update

---
+++ Modul-4/SpreadOperator/SpreadOperator.js
@@ -0,0 +1,5 @@
+const merkHp = ["Oppo", "Realme", "Samsung", "HTC"]
+const merkLainnya = ["Xiaomi", "Andromax", "LG", "Sony Xperia", "Sharp"]
+const semuaMerk = [merkHp, merkLainnya]
+
+console.log(semuaMerk)
