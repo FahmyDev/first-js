8ea4cf5dee640b5c12b61b6c66b2a4f45e6c2968
FahmyDev <atepfahmyy02333@gmail.com>
2023-04-12 14:17:28 +0700

    update tugas

---
+++ Modul-4/Array/Array2.js
@@ -0,0 +1,9 @@
+let arrayNama = ["Bagus", "Fahmi", "Anam", "Rizal", "Tyo", "Aji", "Agung"];
+arrayNama.push('Lutfi Hasan')
+delete arrayNama[4];
+
+let arrayHobi = ["Mancing", "Sepak bola", "Futsal", "Basket", "Membaca Buku"]
+arrayHobi.push('Sepak Takraw')
+delete arrayHobi[0]
+console.log()
+
